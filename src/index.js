2e68aedc2b0ca282f55a77b83d833631e6f66b85
rajatgatade <97111225+rajatgatade@users.noreply.github.com>
2023-05-28 09:23:09 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 //Write your code here
 
+class API {
+  url;
+  method;
+
+  #secure;
+
+  constructor(url, method = 'GET') {
+    this.url = url;
+    this.method = method;
+    this.#secure = url.startsWith('https');
+  }
+
+  isSecure() {
+    return this.#secure;
+  }
+
+  updateUrl(newUrl) {
+    this.url = newUrl;
+    this.#secure = newUrl.startsWith('https');
+  }
+}
+
 
 
 
